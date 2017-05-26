fa7d96446d9385c35a0dcf46f97c2bccba7ac0e3
giuggy <giuggy.rinaldi@gmail.com>
2017-05-26 10:34:51 +0200

    final graphs

--- Throughput.py
+++ Throughput.py
@@ -41,25 +41,22 @@ def destroyLink(matrix, i, j):
     matrix[i, i] = matrix[i, i] - 1
     matrix[j, j] = matrix[j, j] - 1
 
-def linkFail (mat, p, l, n_fail):
+def linkFail (mat, p, l):
     matrix = np.copy(mat)
     failure = 0
-    if n_fail != 0:
-        for i in range(len(mat)):
-            for j in range(len(mat)):
-                if (matrix[i, j] == -1):
-                    k = np.random.choice([0, 1], p = [1 - p, p])
-                    if (k == 1):
-                        destroyLink(matrix, i, j)
-                        failure = failure + 1
-                        if(n_fail == failure):
-                            break
 
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            if (matrix[i, j] == -1):
+                k = np.random.choice([0, 1], p = [1 - p, p])
+                if k == 1:
+                    destroyLink(matrix, i, j)
+                    failure = failure + 1
     thr = throughput(matrix, l)
     return thr
 
 #plot of the Throughput with fix p and variables n and l
-def plotThroughpt (p):
+def plotThroughput_Random (p):
     tot_l = []
     l = [1,2,3,4]
     n = list(np.arange(10, 110, 10))
@@ -82,18 +79,46 @@ def plotThroughpt (p):
     plt.xlabel('Nodes')
     plt.ylabel('Throughput')
     plt.axis([10, 100, 0, 1])
+    plt.title('Reandom Graph')
+    plt.show()
+
+def plotThroughput_Regular( p, r):
+
+    tot_l = []
+    l = [1, 2, 3, 4]
+    n = list(np.arange(10, 110, 10))
+    for i in l:
+        thr = []
+        for j in n:
+            rand_graph = regRandGraph.RegularGraph(j, r, p)
+            mat = rand_graph.build_regular_graph()
+            s = throughput(mat, i)
+            thr.append(s)
+        tot_l.append(thr)
+
+    plt.plot(n, tot_l[0], label="l = 1")
+    plt.plot(n, tot_l[1], label="l = 2")
+    plt.plot(n, tot_l[2], label="l = 3")
+    plt.plot(n, tot_l[3], label="l = 4")
+
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+               ncol=4, mode="expand", borderaxespad=0.)
+
+    plt.xlabel('Nodes')
+    plt.ylabel('Throughput')
+    plt.axis([10, 100, 0, 1])
+    plt.title('Regular Graph')
     plt.show()
 
 #plot throughput with a fix failure
+def plotThroughputFail(mat, prob, title):
 
-def plotThroughputFail( n, p, prob, n_fail):
-    mat = randomGraph.buildRandomGraph(n, p)
     list = []
     l = [1, 3]
     for i in l:
         thr = []
-        for p in prob:
-            t = linkFail(mat, p, i, n_fail)
+        for f in prob:
+            t = linkFail(mat, f, i)
             thr.append(t)
         list.append(thr)
 
@@ -105,7 +130,8 @@ def plotThroughputFail( n, p, prob, n_fail):
 
     plt.xlabel('Probabilities')
     plt.ylabel('Throughput')
-    plt.axis([0.0, 0.25, 0, 1])
+    plt.title(title)
+    plt.axis([0.0, 0.25, 0, 0.5])
     plt.show()
 
 
@@ -113,11 +139,18 @@ def plotThroughputFail( n, p, prob, n_fail):
 def main():
 
     prob = np.arange(0.01, 0.25, 0.01)
-    p = 0.6
+    p = 0.7
+    r = 7
+    plotThroughput_Random(p)
+    plotThroughput_Regular(p, r)
+    n = 20
+    rand_graph = regRandGraph.RegularGraph(n, r, p)
+    mat_r = rand_graph.build_regular_graph()
 
-    plotThroughpt(p)
-    plotThroughputFail(10, p, prob,4)
+    plotThroughputFail(mat_r, prob, 'Regular Graph')
 
+    mat = randomGraph.buildRandomGraph(n, p)
+    plotThroughputFail(mat, prob, 'Random Graph')
 
 
 if __name__ == "__main__":
